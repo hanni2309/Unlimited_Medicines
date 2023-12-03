26fe146224de606068138e5b7dbf419dd379b1be
Hanni <han_2051220136@dau.edu.vn>
2023-12-04 00:58:28 +0700

    create hoàn thành

--- node.js
+++ node.js
@@ -2,10 +2,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/connectMetamask.html"));
-})
+});
 
-const server = app.listen(5000);
-const portNumber = server.address().port;
-console.log(`port is open on ${portNumber}`);
+const server = app.listen(5000, () => {
+    const portNumber = server.address().port;
+    console.log(`Server is running on port ${portNumber}`);
+});
