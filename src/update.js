3a2274fa8b78d360788597b2db140aa935e568bf
hanni2309 <han_2051220136@dau.edu.vn>
2023-12-18 03:21:01 +0700

    khắc phục xong lỗi hiển thị

--- src/update.js
+++ src/update.js
@@ -1,91 +1,74 @@
-// Ensure that Web3 is defined before calling connectContract
-if (typeof window.web3 === 'undefined') {
-    throw new Error('Web3 is not defined. Make sure the Web3 library is loaded.');
+function redirectToUpdateDrug(drugId) {
+    window.location.href = `updateDrug.html?id=${drugId}`;
 }
 
-// Ensure that connectContract is called after Web3 is defined
-window.addEventListener('DOMContentLoaded', (event) => {
-    connectContract();
-    initializeDrugInfo(); // Initialize drug information when the page loads
-});
-
-let initialDrugData = null; // Store the initial drug data
-
-const initializeDrugInfo = async () => {
+// Function to fetch and display the current drug data before updating
+async function displayCurrentDrugData() {
     try {
-        // Ensure window.contract is defined before accessing its methods
-        if (window.contract) {
-            const id = new URLSearchParams(window.location.search).get("id");
-            
-            // Lấy thông tin thuốc dựa trên id từ smart contract
-            const drug = await window.contract.methods.getDrug(id).call({ from: window.ethereum.selectedAddress });
+        // Retrieve the drug ID from the URL parameter
+        const urlParams = new URLSearchParams(window.location.search);
+        const drugId = urlParams.get('id');
 
-            // Lưu trữ dữ liệu thuốc ban đầu
-            initialDrugData = { ...drug };
+        // Fetch the current drug data using the smart contract function
+        const drug = await window.contract.methods.getDrug(drugId).call({ from: window.ethereum.selectedAddress });
 
-            // Hiển thị thông tin thuốc trên giao diện
-            document.getElementById("name").value = drug.name;
-            document.getElementById("classification").value = drug.classification;
-            document.getElementById("expirationDate").value = drug.expirationDate;
-            document.getElementById("productionDate").value = drug.productionDate;
-            document.getElementById("quantity").value = drug.quantity;
-            document.getElementById("location").value = drug.location;
-            document.getElementById("batchNumber").value = drug.batchNumber;
-            document.getElementById("ingredients").value = drug.ingredients;
-            document.getElementById("status").checked = drug.status;
-        } else {
-            console.error("Contract is not properly initialized.");
-        }
+        // Display the current drug data in the update form
+        document.getElementById('name').value = drug.name;
+        document.getElementById('classification').value = drug.classification;
+        document.getElementById('expirationDate').value = drug.expirationDate;
+        document.getElementById('productionDate').value = drug.productionDate;
+        document.getElementById('quantity').value = drug.quantity;
+        document.getElementById('location').value = drug.location;
+        document.getElementById('batchNumber').value = drug.batchNumber;
+        document.getElementById('ingredients').value = drug.ingredients;
+        document.getElementById('status').checked = drug.status;
+
+
     } catch (error) {
-        console.error("Error initializing drug information:", error);
+        console.error("Error fetching current drug data:", error);
     }
-};
+}
 
-const isDataChanged = () => {
-    const currentData = {
-        name: document.getElementById("name").value,
-        classification: document.getElementById("classification").value,
-        expirationDate: document.getElementById("expirationDate").value,
-        productionDate: document.getElementById("productionDate").value,
-        quantity: document.getElementById("quantity").value,
-        location: document.getElementById("location").value,
-        batchNumber: document.getElementById("batchNumber").value,
-        ingredients: document.getElementById("ingredients").value,
-        status: document.getElementById("status").checked,
-    };
-
-    // So sánh dữ liệu hiện tại với dữ liệu ban đầu
-    return JSON.stringify(currentData) !== JSON.stringify(initialDrugData);
-};
-
-const updateDrug = async () => {
+// Function to update the drug on the blockchain
+async function updateDrug() {
     try {
-        // Kiểm tra xem dữ liệu có thay đổi hay không
-        if (!isDataChanged()) {
-            alert("No changes detected. Nothing to update.");
-            return;
-        }
+        // Retrieve the drug ID from the URL parameter
+        const urlParams = new URLSearchParams(window.location.search);
+        const drugId = urlParams.get('id');
 
-        // Thực hiện gọi hàm updateDrug từ smart contract
-        await window.contract.methods.updateDrug(
-            id,
-            name,
-            classification,
-            expirationDate,
-            productionDate,
-            quantity,
-            location,
-            batchNumber,
-            ingredients,
-            status
+      // Get values from the form
+      const name = document.getElementById('name').value;
+      const classification = document.getElementById('classification').value;
+      const expirationDate = document.getElementById('expirationDate').value;
+      const productionDate = document.getElementById('productionDate').value;
+      const quantity = document.getElementById('quantity').value;
+      const location = document.getElementById('location').value;
+      const batchNumber = document.getElementById('batchNumber').value;
+      const ingredients = document.getElementById('ingredients').value;
+      const status = document.getElementById('status').checked;
+
+      // Call the smart contract function to update the drug
+      await window.contract.methods.updateDrug(
+          drugId,
+          name,
+          classification,
+          expirationDate,
+          productionDate,
+          quantity,
+          location,
+          batchNumber,
+          ingredients,
+          status
+            // Pass other input values
         ).send({ from: window.ethereum.selectedAddress });
-        // (Code gọi hàm updateDrug đã được giữ nguyên)
-        // ...
+
+        // Redirect to the show.html page after successful update
+        window.location.href = "showDrug.html";
 
     } catch (error) {
         console.error("Error updating drug:", error);
     }
-};
+}
 
-// Gọi hàm initializeDrugInfo khi tải trang
-initializeDrugInfo();
+// Call the displayCurrentDrugData function when the page loads
+window.onload = displayCurrentDrugData;
